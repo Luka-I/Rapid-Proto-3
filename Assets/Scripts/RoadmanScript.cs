76bcee0ca2c2a7cd562d562700854d3a28e9b53f
Luka-I <s2300827@edu.bc.fi>
2025-10-03 10:16:51 +0300

    Scriptaus alku

--- Assets/Scripts/RoadmanScript.cs
+++ Assets/Scripts/RoadmanScript.cs
@@ -1,11 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class RoadmanScript : MonoBehaviour
 {
+
+    public float speed = 5f; // Speed of the roadman
+    public int health = 100; // Health of the roadman
+    public int damage = 10; // Damage dealt by the roadman
+    public Transform player; // Reference to the player transform
+    public float attackRange = 2f; // Range within which the roadman can attack
+    public float detectionRange = 10f; // Range within which the roadman can detect the player
+    public float attackCooldown = 1f; // Cooldown time between attacks
+
+    private Animator anim;
+    private enum State { Idle, Chasing, Attacking, Recovering}
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
     // Update is called once per frame
